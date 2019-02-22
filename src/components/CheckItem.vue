e001e7fc567ff9d0fd3899340311f49e0bbcc523
kamaboko <nerorubymoon@gmail.com>
2019-02-22 14:07:39 +0900

    :rocket:

--- src/components/CheckItem.vue
+++ src/components/CheckItem.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <el-row>
-      <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
-        <el-button class="is-full-width">
-          {{ check.name }}
-          <el-switch
-            v-model="isDone"
-            active-color="#13ce66"
-            inactive-color="#ff4949"
-          >
-          </el-switch>
-        </el-button>
-      </el-col>
-    </el-row>
+    <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+      <el-button class="is-full-width" round @click="done">
+        {{ check.name }}
+        <el-switch
+          v-model="isDone"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+      </el-button>
+    </el-col>
   </div>
 </template>
 <script>
@@ -29,18 +27,21 @@ export default {
     return {
       isDone: false
     };
+  },
+  methods: {
+    done: function() {
+      if (this.isDone) {
+        this.isDone = false;
+      } else {
+        this.isDone = true;
+      }
+    }
   }
 };
 </script>
 <style>
 .el-row {
   margin-bottom: 20px;
-}
-.el-row:last-child {
-  margin-bottom: 0;
-}
-.el-row:first-child {
-  background-color: #f9fafc;
 }
 .el-col {
   border-radius: 4px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
     <section>
-      <article v-for="check in checkList" :key="check.id">
-        <check-item :check="check"></check-item>
+      <article>
+        <el-row v-for="check in checkList" :key="check.id">
+          <check-item :check="check"></check-item>
+        </el-row>
+      </article>
+    </section>
+    <section>
+      <article>
+        <el-row>
+          <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
+            <el-button type="success" icon="el-icon-check" round>
+              終わった！
+            </el-button>
+          </el-col>
+        </el-row>
       </article>
     </section>
   </div>
